fab09e7c0e5e06f6bb9f7670f0ec7eaddc016258
sokkejimbo <emilholm2005@gmail.com>
2023-03-15 13:28:44 +0100

    Update datastuff.py

--- CanSat/Python/datastuff.py
+++ CanSat/Python/datastuff.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open('D:\LOG00014.txt') as f:
+with open('C:\cansat\data.txt') as f:
     lines = f.readlines()
 
 time=[]
@@ -10,12 +10,12 @@ bmpTemp=[]
 bmpTryk=[]
 bmpAlt=[]
 
-for i in range(0, len(lines), 1):
+for i in range(550, len(lines), 1):
     time.append(int(lines[i].split()[1]))
     ntcTempRaw.append(int(lines[i].split()[2]))
-    bmpTemp.append(float(lines[i].split()[3]))
+    """bmpTemp.append(float(lines[i].split()[3]))
     bmpTryk.append(float(lines[i].split()[4]))
-    bmpAlt.append(float(lines[i].split()[5]))
+    bmpAlt.append(float(lines[i].split()[5]))"""
 
-plt.plot(time, bmpTemp)
+plt.plot(time, ntcTempRaw)
 plt.show()
